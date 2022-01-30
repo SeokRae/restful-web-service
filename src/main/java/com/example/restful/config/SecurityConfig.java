68e2d093c963a848262f0fd7dfd4b6b6e4ac251c
seokrae <kslbsh@gmail.com>
2022-01-30 19:37:38 +0900

    feat: security 설정 및 사용자 권한 예제 추가

--- src/main/java/com/example/restful/config/SecurityConfig.java
+++ src/main/java/com/example/restful/config/SecurityConfig.java
@@ -1,7 +1,17 @@
 package com.example.restful.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("seokrae")
+                .password("{noop}1234")
+                .roles("USER");
+    }
 }
