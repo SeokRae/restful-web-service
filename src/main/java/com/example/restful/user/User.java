fd32e363519194c969eae681d1dcc5c4c63a4082
seokrae <kslbsh@gmail.com>
2022-01-30 18:43:12 +0900

    feat: swagger 간단하게 추가 하는 방법 (의존성 버전 주의)

--- src/main/java/com/example/restful/user/User.java
+++ src/main/java/com/example/restful/user/User.java
@@ -1,5 +1,7 @@
 package com.example.restful.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,13 +14,18 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
 
     private Integer id;
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     @Size(min = 2, message = "name의 길이는 2자 이상 입력해주세요.")
     private String name;
+    @ApiModelProperty(notes = "사용자의 등록일을 입력해 주세요.")
     @Past
     private Date joinDate;
+    @ApiModelProperty(notes = "사용자의 패스워드를 입력해 주세요.")
     private String password;
+    @ApiModelProperty(notes = "사용자의 주민번호를 입력해 주세요.")
     private String ssn;
 }
