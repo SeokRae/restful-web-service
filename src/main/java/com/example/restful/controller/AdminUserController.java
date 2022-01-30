98693833a4a7a26404158182f37472e6c7098c13
seokrae <kslbsh@gmail.com>
2022-01-30 17:27:45 +0900

    feat: mime type versioning

--- src/main/java/com/example/restful/controller/AdminUserController.java
+++ src/main/java/com/example/restful/controller/AdminUserController.java
@@ -50,7 +50,16 @@ public class AdminUserController {
 
     // curl -i -X GET http://localhost:8888/admin/v1/users/1
     // GET /users/1
-    @GetMapping(path = "/users/{id}", params = "version=1")
+    // @GetMapping(path = "/v1/users/{id}", params = "version=1")
+    // curl -i -X GET http://localhost:8888/admin/users/1/?version=1
+    // @GetMapping(path = "/users/{id}", params = "version=1")
+    // curl -i -X GET http://localhost:8888/admin/users/1 \
+    //      -H 'X-API-VERSION: 1'
+    // @GetMapping(path = "/users/{id}", headers = "X-API-VERSION=1")
+    // MIME 타입을 이용하는 방법
+    // curl -i -X GET http://localhost:8888/admin/users/1/ \
+    // -H 'Accept: application/vnd.company.appv1+json'
+    @GetMapping(path = "/users/{id}", produces = "application/vnd.company.appv1+json")
     public MappingJacksonValue getUser(@PathVariable Integer id) {
         User findUser = userDaoService.findById(id);
         if (findUser == null) {
@@ -68,10 +77,19 @@ public class AdminUserController {
 
         return mappingJacksonValue;
     }
-    // curl -i -X GET http://localhost:8888/admin/v2/users/1
+
     // GET /users/1
+    // curl -i -X GET http://localhost:8888/admin/v2/users/1
     // @GetMapping(path = "/v2/users/{id}")
-    @GetMapping(path = "/users/{id}", params = "version=2")
+    // curl -i -X GET http://localhost:8888/admin/users/1/?version=2
+    // @GetMapping(path = "/users/{id}", params = "version=2")
+    // curl -i -X GET http://localhost:8888/admin/users/1/ \
+    // -H 'X-API-VERSION: 2'
+    // @GetMapping(path = "/users/{id}", headers = "X-API-VERSION=2")
+    // MIME 타입을 이용하는 방법
+    // curl -i -X GET http://localhost:8888/admin/users/1/ \
+    // -H 'Accept: application/vnd.company.appv2+json'
+    @GetMapping(path = "/users/{id}", produces = "application/vnd.company.appv2+json")
     public MappingJacksonValue getUserV2(@PathVariable Integer id) {
         User findUser = userDaoService.findById(id);
         if (findUser == null) {
